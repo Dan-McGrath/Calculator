4fed818bc72a75782f33244786952ec39495e169
Dan-McGrath <mcgrathd3254@yahoo.com>
2023-05-22 22:53:56 -0400

    Add functions to get values

--- app.js
+++ app.js
@@ -23,3 +23,56 @@ const divide = (num1, num2) => {
     return answer.toFixed(5);
     
 }
+const current = document.querySelector('.current');
+const expression = document.querySelector('.expression');
+let operator;
+
+const getOperator = (e) => {
+    operator = e.target.dataset.operation;
+    expression.textContent = `${current.textContent} ${operator}`;
+    return operator
+}
+
+const getNumber = (e) => {
+    num = e.target.dataset.number;
+    current.textContent = num
+    return num
+}
+
+
+
+
+
+
+// const operate = (num1, operator, num2) => {
+//     if (operator.dataset.operation === 'add') {
+//         return add(num1, num2);
+//     } else if(operator.dataset.operation === 'subtract') {
+//        return subtract(num1, num2);
+//     } else if(operator.dataset.operation === 'multiply') {
+//         return multiply(num1, num2);
+//     } else if(operator.dataset.operation === 'divide') {
+//         return divide(num1, num2);
+//     } else if(operator.dataset.operation === 'equal') {
+//         return num1
+//     }
+// }
+
+
+
+const number = document.querySelectorAll('.number');
+const operation = document.querySelectorAll('.operation');
+
+const addClickOperator = () => {
+    for (let i = 0; i < operation.length; i++) {
+        operation[i].addEventListener('click', getOperator) 
+    }
+    
+}
+const addClickNumber = () => {
+    for(let i = 0; i < number.length; i++) {
+        number[i].addEventListener('click', getNumber)
+    }
+}
+addClickNumber();
+addClickOperator();
